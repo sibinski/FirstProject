14a76dce62845575750ae9f09333d28e09cac84b
sibinski <jevgeni.shibinski@gmail.com>
2024-06-30 08:30:11 +0300

    Updated Landing Page for Little Lemon, added ScrollView

--- components/LittleLemonHeader.js
+++ components/LittleLemonHeader.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { View, Text } from 'react-native';
 export default function LittleLemonHeader() {
     return (
-        <View style={{ flex: 0.30, backgroundColor: '', marginBottom: 50 }}>
+        <View style={{ flex: 0.30, backgroundColor: '', marginBottom: 30 }}>
             <Text style={{ textAlign: 'center', padding: 50, fontSize: 50, color: 'black' }} numberOfLines={3}>
                 Welcome to <br></br>
                 <Text style={{ fontWeight: 'bold' }}> The Little Lemon </Text>  {''}
--- components/Welcome.js
+++ components/Welcome.js
@@ -1,19 +1,21 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { Text, ScrollView } from 'react-native';
 
 export default function Welcome() {
   return (
-    <View style={{ flex: 0.7, backgroundColor: '#F4CE14', marginLeft: 250, marginRight: 250, borderRadius: 50 }}>
+    <ScrollView indicatorStyle={"white"} style={{ flex: 0.7, backgroundColor: '#F4CE14', borderRadius: 100, marginLeft: 250, marginRight: 250, paddingHorizontal: 40,
+        paddingVertical: 40, }}>
       <Text
         style={{
-          marginTop: 200,
+          marginTop: 100,
           fontStyle: 'italic', 
           fontSize: 18,
           color: 'black',
-          textAlign: 'center',
+          textAlign: 'justify',
         }}>
-        Little Lemon is a charming neighborhood bistro that serves simple food and classic cocktails<br></br> in a lively but casual environment.<br></br> We would love to hear more about your experience with us!
+Little Lemon is a charming neighborhood bistro that serves simple food and classic cocktails in a lively but casual environment. We would love to hear more about your experience with us! 
+Thank you for choosing Little Lemon for your dining experience.
       </Text>
-    </View>
+      </ScrollView>
   );
 }
